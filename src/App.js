d1814ad77805f6ecd5e685200ff4e84cd96b4f9b
Amilcar_Sandoval <amilcarbaltodano@gmail.com>
2021-12-14 22:00:40 -0600

    Diseño y validacion de usuario

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* CODIGO PARA MANDAR A LLAMAR Navbar y Product
 import './App.css';
 import Product from './Conponet/Product';
 import Navbar from './Conponet/Navbar';
@@ -8,6 +9,26 @@ function App() {
       <Navbar/>
       <Product/>
     </div>
+  );
+}
+
+export default App;
+*/
+
+//import Login from './Conponet/Login/Login';
+//import Main from './Login/Main' ; 
+
+import Login from './Login/Login'
+const URL_API_LOGIN = "http://localhost/webservices/login/login.php";
+
+function App() {
+
+   //Resivimos el valor y lo imprimimos en consola
+  const desbloquear = (valor)=>{
+    console.log(valor);
+  }
+  return (
+    <Login urlApi={URL_API_LOGIN} desbloquear={desbloquear} />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//Agregamos la importacion del bootstrap
+import './css/bootstrap.min.css';
+//import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
